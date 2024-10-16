47ed4b1fd25180173959d9236c9e49f913b9381c
OmaymaMansour <omaymamansour26@gmail.com>
2024-10-16 21:28:34 +0300

    add launch screen and enhance home screen

--- SelfieTestApp/Home/Controller/HomeViewController.swift
+++ SelfieTestApp/Home/Controller/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  SelfieTestApp
 //
 //  Created by Omayma Marouf on 11/10/2024.
@@ -8,22 +8,11 @@
 import UIKit
 import ValifySelfieFramework  // Import the framework
 
-class ViewController: UIViewController {
-    
-    @IBOutlet weak var selfieBtn: UIButton!
-    @IBOutlet weak var titleLbl: UILabel!
-    private var capturedImage: UIImage? // Store the captured image
+class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupInitialUI() // Set up the initial UI state
     }
-    
-    // Function to set up the initial UI elements
-        private func setupInitialUI() {
-            titleLbl.isHidden = true // Hide the title label initially
-            selfieBtn.setTitle("Capture Selfie", for: .normal) // Set the initial title for the selfie button
-        }
     
     
     @IBAction func launchCamera(_ sender: Any) {
@@ -36,7 +25,7 @@ class ViewController: UIViewController {
 
 // MARK: - ValifySelfieFrameworkDelegate
 // Extension to handle delegate methods for capturing selfies
-extension ViewController: ValifySelfieFrameworkDelegate {
+extension HomeViewController: ValifySelfieFrameworkDelegate {
     // Method called when an image is captured
     func didCaptureImage(_ image: UIImage) {
         // Dismiss the camera view controller before showing the image preview
@@ -46,28 +35,34 @@ extension ViewController: ValifySelfieFrameworkDelegate {
             // Instantiate the image preview view controller for approval
             let previewVC = ImagePreviewViewController(image: image)
             previewVC.delegate = self // Set the delegate to handle approval or recapture
-            self.present(previewVC, animated: true) // Present the image preview controller
+            self.present(previewVC, animated: true)
         }
     }
 }
 
 // MARK: - ImagePreviewDelegate
 // Extension to handle delegate methods for image preview
-extension ViewController: ImagePreviewDelegate {
+extension HomeViewController: ImagePreviewDelegate {
     // Method called when the user approves the captured image
     func didApproveImage(_ image: UIImage) {
-        // Show the title label and update its text to indicate success
-        titleLbl.isHidden = false
-        titleLbl.text = "Sent Successfully" // Update the label text
-        
-        // Change the button title to indicate the user can take another selfie
-        selfieBtn.setTitle("Take a Selfie", for: .normal)
-        
-        // Notify the user that the selfie has been approved using an alert
-        let alert = UIAlertController(title: "Selfie Approved", message: "Your selfie has been approved!", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default)) // Add an "OK" action to the alert
-        present(alert, animated: true) // Present the alert
+        // Dismiss any currently presented view controller before showing a new alert
+        if let presentedVC = self.presentedViewController {
+            presentedVC.dismiss(animated: false) { [weak self] in
+                self?.showApprovalAlert()
+            }
+        } else {
+            showApprovalAlert()
+        }
     }
+
+    private func showApprovalAlert() {
+        // Create and present the alert confirming approval
+        let alert = UIAlertController(title: "Selfie Approved", message: "Your selfie has been approved!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+        }))
+        present(alert, animated: true)
+    }
+
     
     // Method called when the user requests to recapture the selfie
     func didRequestRecapture() {
